872863b2a11b97421f078a73325900e3676af86c
Sam Sorensen <825813+sabrsorensen@users.noreply.github.com>
2021-09-07 12:51:23 -0600

    Changed orange to cyan-ish for class names.

--- src/js/theme_template.js
+++ src/js/theme_template.js
@@ -18,22 +18,27 @@
     if (!disableGlow) {
       /* replace orange */
       /*updatedThemeStyles = updatedThemeStyles.replace(/color: #ecc48d;/g, "color: #ecc48d; text-shadow: 0 0 2px #eecea1, 0 0 3px #ebd1ad[NEON_BRIGHTNESS], 0 0 5px #f3e8d9[NEON_BRIGHTNESS];"); */
+
+      /* replace red */
       updatedThemeStyles = updatedThemeStyles.replace(/color: #d64a53;/g, "color: #ff3341; text-shadow: 0 0 2px #ff4a00, 0 0 3px #fc1f2c[NEON_BRIGHTNESS], 0 0 5px #fc1f2c[NEON_BRIGHTNESS];");
 
       /* replace yellow */
       updatedThemeStyles = updatedThemeStyles.replace(/color: #fede5d;/g, "color: #f9faaa; text-shadow: 0 0 2px #f9faaa, 0 0 3px #f39f05[NEON_BRIGHTNESS], 0 0 5px #f39f05[NEON_BRIGHTNESS], 0 0 8px #f39f05[NEON_BRIGHTNESS];");
 
-      /* UPDATED replace green */
+      /* replace green */
       updatedThemeStyles = updatedThemeStyles.replace(/color: #7fdbca;/g, "color: #7fdbca; text-shadow: 0 0 2px #100c0f, 0 0 5px #17f1c9[NEON_BRIGHTNESS], 0 0 8px #52e6cb[NEON_BRIGHTNESS];");
 
-      /* UPDATED replace pink */
+      /* replace pink */
       updatedThemeStyles = updatedThemeStyles.replace(/color: #c792ea;/g, "color: #c792ea; text-shadow: 0 0 2px #001716, 0 0 3px #ae46f3[NEON_BRIGHTNESS], 0 0 5px #ba6eec[NEON_BRIGHTNESS], 0 0 8px #c792ea[NEON_BRIGHTNESS];");
 
       /* replace periwinkle */
-      updatedThemeStyles = updatedThemeStyles.replace(/color: #82AAFF;/g, "color: #82AAFF; text-shadow: 0 0 2px #001716, 0 0 3px #3370f3[NEON_BRIGHTNESS], 0 0 5px #4e83f5[NEON_BRIGHTNESS], 0 0 8px #8badf5[NEON_BRIGHTNESS];");
+      updatedThemeStyles = updatedThemeStyles.replace(/color: #82aaff;/g, "color: #82aaff; text-shadow: 0 0 2px #001716, 0 0 3px #3370f3[NEON_BRIGHTNESS], 0 0 5px #4e83f5[NEON_BRIGHTNESS], 0 0 8px #8badf5[NEON_BRIGHTNESS];");
 
       /* UPDATED replace light grey */
       //updatedThemeStyles = updatedThemeStyles.replace(/color: #d6deeb;/g, "color: #d6deeb; text-shadow: 0 0 2px #001716, 0 0 3px #d6deeb[NEON_BRIGHTNESS], 0 0 5px #d6deeb[NEON_BRIGHTNESS], 0 0 8px #d6deeb[NEON_BRIGHTNESS];");
+
+      /* replace teal */
+      updatedThemeStyles = updatedThemeStyles.replace(/color: #57eaf1;/g, "color: #3fe7ef; text-shadow: 0 0 2px #100c0f, 0 0 5px #57eaf1[NEON_BRIGHTNESS], 0 0 8px #86f0f5[NEON_BRIGHTNESS];");
     }
 
     /* append the remaining styles */
